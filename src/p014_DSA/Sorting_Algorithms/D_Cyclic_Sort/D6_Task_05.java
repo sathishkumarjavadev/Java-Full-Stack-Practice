4128e60a8fd7bb6df02b33fcc9dccbcee3f7b57a
Sathish Kumar <sathishkumar.javadev@gmail.com>
2026-09-06 23:19:28 +0530

    Add Set Mismatch solution using Cyclic Sort

---
+++ src/p014_DSA/Sorting_Algorithms/D_Cyclic_Sort/D6_Task_05.java
@@ -0,0 +1,45 @@
+package p014_DSA.Sorting_Algorithms.D_Cyclic_Sort;
+
+import java.util.Arrays;
+
+//set mismatch
+public class D6_Task_05 {
+	
+	public static void main(String[] args) {
+		int[] arr= {1,2,2,4};
+		System.out.println(Arrays.toString(findErrorNums(arr)));
+	}
+	static int[] findErrorNums(int[] arr)
+	{
+		int i=0;
+		while(i<arr.length)
+		{
+			int correct =arr[i]-1;
+			if(arr[i] != arr[correct])
+			{
+				swap(arr, i, correct);
+			}
+			else
+			{
+				i++;
+			}
+		}
+		
+		//search for the first missing number
+		for(int index =0;index<arr.length;index++)
+		{
+			if(arr[index]!=index+1)
+			{
+				return new int[] {arr[index],index+1};
+			}
+	}
+		return new int[] {-1,-1};
+		}
+		static void swap(int [] arr, int first , int second)
+		{
+			int temp = arr[first];
+			arr[first] = arr[second];
+			arr[second] = temp;
+		}
+	
+}
