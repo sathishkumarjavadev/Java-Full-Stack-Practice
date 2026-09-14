369c765dc86af1241c50c2577446fc8b86af4b3e
Sathish Kumar <sathishkumar.javadev@gmail.com>
2026-09-14 11:03:30 +0530

    Add recursion and problem solving practice

---
+++ src/p017_Tasks_and_Practice/T1_Basics/Sum.java
@@ -0,0 +1,15 @@
+package p017_Tasks_and_Practice.T1_Basics;
+
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		int a, b, sum;
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter the a and b values");
+		a= obj.nextInt();
+		b = obj.nextInt();
+		sum=a+b;
+		System.out.println("sum ="+sum);
+	}
+}
